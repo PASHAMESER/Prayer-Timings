9fd55286165e2e23def808573a4da76a6b72e882
Mohamed Roshdy <62522577+PASHAMESER@users.noreply.github.com>
2023-10-05 09:41:09 +0300

    s

--- main.js
+++ main.js
@@ -2,68 +2,68 @@
 
 let cities = [
   {
-  arabicName : "الفيوم",
-  name : "Faiyum"
+    arabicName: "الفيوم",
+    name: "Faiyum",
   },
   {
-  arabicName : "القاهرة",
-  name : "Cairo"
+    arabicName: "القاهرة",
+    name: "Cairo",
   },
   {
-  arabicName : "بني سويف",
-  name : "Beni Suef"
+    arabicName: "بني سويف",
+    name: "Beni Suef",
   },
   {
-  arabicName : "الاسكندرية",
-  name : "Alexandria"
-  }
-]
+    arabicName: "الاسكندرية",
+    name: "Alexandria",
+  },
+];
 
-for(city of cities) {
+for (city of cities) {
   let content = `
   <option>${city.arabicName}</option>
-  `
-  document.getElementById("btn-selected").innerHTML += content
+  `;
+  document.getElementById("btn-selected").innerHTML += content;
 }
-document.getElementById("btn-selected").addEventListener("change" , function(){
-  document.getElementById("city").innerHTML = this.value
-  let cityName = ""
-  for(let city of cities){
-    if(city.arabicName == this.value){
-      cityName = city.name
+document.getElementById("btn-selected").addEventListener("change", function () {
+  document.getElementById("city").innerHTML = this.value;
+  let cityName = "";
+  for (let city of cities) {
+    if (city.arabicName == this.value) {
+      cityName = city.name;
     }
   }
-  getPrayersTimingsOfCity(cityName)
-})
+  getPrayersTimingsOfCity(cityName);
+});
 
-function getPrayersTimingsOfCity(cityName){
+function getPrayersTimingsOfCity(cityName) {
   let params = {
-    country : "eg",
-    city : cityName
-  }
+    country: "eg",
+    city: cityName,
+  };
 
-  axios.get('http://api.aladhan.com/v1/timingsByCity', {params: params})
-  .then(function (response) {
-    const timings = response.data.data.timings
-    fillTimeForPrayer("Fajr-time",timings.Fajr)
-    fillTimeForPrayer("Sunrise-time",timings.Sunrise)
-    fillTimeForPrayer("Dhuhr-time",timings.Dhuhr)
-    fillTimeForPrayer("Asr-time",timings.Asr)
-    fillTimeForPrayer("Maghrib-time",timings.Maghrib)
-    fillTimeForPrayer("Isha-time",timings.Isha)
-    let dateDAY_M = response.data.data.date.readable;
-    let dateDAY_AR = response.data.data.date.hijri.weekday.ar;
-  
-  
-    document.getElementById("day-time").innerHTML =  dateDAY_AR + " " + dateDAY_M
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  
+  axios
+    .get("http://api.aladhan.com/v1/timingsByCity", { params: params })
+    .then(function (response) {
+      const timings = response.data.data.timings;
+      fillTimeForPrayer("Fajr-time", timings.Fajr);
+      fillTimeForPrayer("Sunrise-time", timings.Sunrise);
+      fillTimeForPrayer("Dhuhr-time", timings.Dhuhr);
+      fillTimeForPrayer("Asr-time", timings.Asr);
+      fillTimeForPrayer("Maghrib-time", timings.Maghrib);
+      fillTimeForPrayer("Isha-time", timings.Isha);
+      let dateDAY_M = response.data.data.date.readable;
+      let dateDAY_AR = response.data.data.date.hijri.weekday.ar;
+
+      document.getElementById("day-time").innerHTML =
+        dateDAY_AR + " " + dateDAY_M;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 
-function fillTimeForPrayer(id,time){
-  document.getElementById(id).innerHTML = time
+function fillTimeForPrayer(id, time) {
+  document.getElementById(id).innerHTML = time;
 }
-getPrayersTimingsOfCity("Faiyum")
+getPrayersTimingsOfCity("Faiyum");
